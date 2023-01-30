16fc0d70ec76a3a072f664b8df2ad9f558c3b6f1
jeremyraby <jeremy.raby10@gmail.com>
2023-01-30 09:44:50 -0700

    Correct unquoted table names for PostgresSQL and change to INNER JOIN.

--- netflix.sql
+++ netflix.sql
@@ -1,6 +1,15 @@
 -- Example query (PostgreSQL)
 -- SELECT * FROM "CharlotteChaze/BreakIntoTech"."netflix_people";
 
+SELECT 
+    "CharlotteChaze/BreakIntoTech"."netflix_titles_info"."title" AS "Title",
+    "CharlotteChaze/BreakIntoTech"."netflix_people"."director" AS "Director",
+    "CharlotteChaze/BreakIntoTech"."netflix_titles_info"."rating" AS "MPAA Rating"
+FROM "CharlotteChaze/BreakIntoTech"."netflix_titles_info"
+INNER JOIN "CharlotteChaze/BreakIntoTech"."netflix_people"
+ON "CharlotteChaze/BreakIntoTech"."netflix_titles_info"."show_id" = "CharlotteChaze/BreakIntoTech"."netflix_people"."show_id";
+
+
 -- Tips:
 -- (*) Table names containing punctuation, capitalization, or spaces should be
 --     quoted, e.g. "Test Data.csv".
@@ -9,12 +18,3 @@
 -- (*) When querying tables created outside of the public (default) schema,
 --     be sure to specify the desired schema, e.g. "SELECT * FROM my_schema."my data.csv";".
 
-SELECT 
-    "CharlotteChaze/BreakIntoTech"."netflix_titles_info"."title" AS Title,
-    "CharlotteChaze/BreakIntoTech"."netflix_people"."director" AS Director,
-    "CharlotteChaze/BreakIntoTech"."netflix_titles_info"."rating" AS "MPAA Rating"
-FROM "CharlotteChaze/BreakIntoTech"."netflix_titles_info"
-JOIN "CharlotteChaze/BreakIntoTech"."netflix_people"
-ON 
-    "CharlotteChaze/BreakIntoTech"."netflix_titles_info"."show_id" = 
-    "CharlotteChaze/BreakIntoTech"."netflix_people"."show_id";
